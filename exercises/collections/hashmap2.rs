a08f09c227206683bc2fb63049a7e880caf8305f
Florian Schramm <flo@geony.de>
2022-06-04 19:41:41 +0200

    solved until quiz2

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,8 +12,6 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -35,9 +33,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
-        // are not allowed to put any type of fruit that's already
-        // present!
+        *basket.entry(fruit).or_insert(0);
     }
 }
 
@@ -74,6 +70,9 @@ mod tests {
     #[test]
     fn greater_than_eleven_fruits() {
         let mut basket = get_fruit_basket();
+
+        basket.insert(Fruit::Pineapple, 5);
+
         fruit_basket(&mut basket);
         let count = basket.values().sum::<u32>();
         assert!(count > 11);
